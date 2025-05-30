4a36751794af4f3bed2a9c4546fc115ab2d34f42
AndreSalas <andre.salas.cr.201@gmail.com>
2025-05-30 00:06:54 -0600

    Se arreglan errores de nombres de scripts

--- Proyecto1/assets/scripts/scenes.lua
+++ Proyecto1/assets/scripts/scenes.lua
@@ -7,10 +7,10 @@ scenes = {
     {name = "level_01_win", path = "assets/scripts/scene_01_Win.lua"},
     {name = "level_02", path = "assets/scripts/scene_02.lua"},
     {name = "level_02_2p", path = "assets/scripts/scene_02_2p.lua"},
-    {name = "level_02_lose", path = "assets/scripts/scene_02_lose.lua"},
-    {name = "level_02_win", path = "assets/scripts/scene_02_win.lua"},
+    {name = "level_02_lose", path = "assets/scripts/scene_02_Lose.lua"},
+    {name = "level_02_win", path = "assets/scripts/scene_02_Win.lua"},
     {name = "level_03", path = "assets/scripts/scene_03.lua"},
     {name = "level_03_2p", path = "assets/scripts/scene_03_2p.lua"},
-    {name = "level_03_lose", path = "assets/scripts/scene_03_lose.lua"},
-    {name = "level_03_win", path = "assets/scripts/scene_03_win.lua"}
+    {name = "level_03_lose", path = "assets/scripts/scene_03_Lose.lua"},
+    {name = "level_03_win", path = "assets/scripts/scene_03_Win.lua"}
 }
